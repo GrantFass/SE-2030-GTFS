c36b6a54d78392543723cfd60956a4f6ce17c73c
beckerrMSOE <beckerr@msoe.edu>
2020-10-18 15:01:27 -0500

    update RouteTypeEnum - Simplified getValue(int value)

--- src/SE2030TransitProject/RouteTypeEnum.java
+++ src/SE2030TransitProject/RouteTypeEnum.java
@@ -56,28 +56,8 @@ public enum RouteTypeEnum {
      * return the enumerator value associated with the specified integer value
      * @param value the value to use
      * @return the enumerator value associated with the integer or the default if none match
-     * @author Grant Fass
+     * @author Grant Fass, Ryan Becker
      */
     public static RouteTypeEnum getValue(int value) {
-        if (value == 0) {
-            return STREETCAR;
-        } else if (value == 1) {
-            return SUBWAY;
-        } else if (value == 2) {
-            return RAIL;
-        } else if (value == 4) {
-            return FERRY;
-        } else if (value == 5) {
-            return CABLE_CAR;
-        } else if (value == 6) {
-            return AERIAL_LIFT;
-        } else if (value == 7) {
-            return FUNICULAR;
-        } else if (value == 8) {
-            return TROLLEYBUS;
-        } else if (value == 12) {
-            return MONORAIL;
-        } else {
-            return BUS;
-        }
+        return values()[value];
     }}
