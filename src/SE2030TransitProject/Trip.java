ccf2d083922f5af48dc1156c6ebf9b690a56cc9d
ericksons <ericksons@msoe.edu>
2020-10-19 22:58:30 -0500

    trips export

--- src/SE2030TransitProject/Trip.java
+++ src/SE2030TransitProject/Trip.java
@@ -50,27 +50,11 @@ public class Trip {
         this.shape_id = shape_id;
 
         //checking service_id is a day of the week
-        try {
-            this.service_id = service_id;
-            if (!(this.service_id.equalsIgnoreCase("SUNDAY")
-                    || this.service_id.equalsIgnoreCase("MONDAY")
-                    || this.service_id.equalsIgnoreCase("TUESDAY")
-                    || this.service_id.equalsIgnoreCase("WEDNESDAY")
-                    || this.service_id.equalsIgnoreCase("THURSDAY")
-                    || this.service_id.equalsIgnoreCase("FRIDAY")
-                    || this.service_id.equalsIgnoreCase("SATURDAY")
-                    || this.service_id.equalsIgnoreCase("WEEKDAY")
-                    || this.service_id.equalsIgnoreCase("WEEKEND"))) {
-                throw new IllegalArgumentException();
-            }
-            //Set enumerator values (default values are applied if empty).
-            this.bikes_allowed = BikesAllowedEnum.getValue(!bikes_allowed.isEmpty() ? Integer.parseInt(bikes_allowed) : -1);
-            this.direction_id = DirectionIDEnum.getValue(!direction_id.isEmpty() ? Integer.parseInt(direction_id) : -1);
-            this.wheelchair_accessible = WheelchairAccessibleEnum.getValue(!wheelchair_accessible.isEmpty() ? Integer.parseInt(wheelchair_accessible) : -1);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("service_id value must be SUNDAY/ MONDAY/ " +
-                    "TUESDAY/ WEDNESDAY/ THURSDAY/ FRIDAY/ SATURDAY/ WEEKDAY/ WEEKEND. Skipping!");
-        }
+        this.service_id = service_id;
+        //Set enumerator values (default values are applied if empty).
+        this.bikes_allowed = BikesAllowedEnum.getValue(!bikes_allowed.isEmpty() ? Integer.parseInt(bikes_allowed) : -1);
+        this.direction_id = DirectionIDEnum.getValue(!direction_id.isEmpty() ? Integer.parseInt(direction_id) : -1);
+        this.wheelchair_accessible = WheelchairAccessibleEnum.getValue(!wheelchair_accessible.isEmpty() ? Integer.parseInt(wheelchair_accessible) : -1);
     }
 
     /**
@@ -181,5 +165,24 @@ public class Trip {
                         "Trip Headsign: %s\n\t" +
                         "Wheelchair Accessible: %s\n", trip_id, bikes_allowed, block_id,
                 direction_id, route_id, service_id, shape_id, trip_headsign, wheelchair_accessible);
+    }
+
+    /**
+     * gets the file header for Trips
+     * @return file header in a single line header format
+     * @author Simon Erickson
+     */
+    public static String getHeaderLine() {
+        return "route_id,service_id,trip_id,trip_headsign,direction_id,block_id,shape_id";
+    }
+
+    /**
+     * gets the data line for a single trip for trips
+     * @return trip data in a single line data format
+     * @author Simon Erickson
+     */
+    public String getDataLine() {
+        return String.format("%s,%s,%s,%s,%s,%s,%s\n", route_id,service_id,trip_id,trip_headsign,
+                direction_id.getValue(),block_id,shape_id);
     }
 }//end Trip
