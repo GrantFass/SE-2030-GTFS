4538fb519a057c11acda805b32a0db78fb753979
Grant Fass <GrantFass@Gmail.com>
2020-11-04 15:59:46 -0600

    Implement Route and Bus Selection
     - Update the map window to only display the currently selected
       route and bus from a list of all of the routes and all of the
       busses. Moved some data gathering methods to the Data class.
    
    Ref #33

--- src/gui/MapWindowController.java
+++ src/gui/MapWindowController.java
@@ -13,21 +13,15 @@ import data.Route;
 import data.Stop;
 import data.Trip;
 import interfaces.Observer;
-import javafx.application.Platform;
-import javafx.concurrent.Task;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -39,13 +33,15 @@ import java.util.stream.Collectors;
  */
 public class MapWindowController implements Observer {
     @FXML
+    private ListView routeListView;
+    @FXML
+    private ListView busListView;
+    @FXML
     private Label labelCenter;
     @FXML
     private Label labelZoom;
     @FXML
     private MapView map;
-    @FXML
-    private VBox primaryVBox;
     @FXML
     private TextArea description;
     private Stage analysisWindowStage;
@@ -181,135 +177,72 @@ public class MapWindowController implements Observer {
                 "Import Window Help", "Not Implemented Yet");
     }
 
-    /**
-     * plots a single coordinate as a marker onto the map
-     * @param latitude the latitude to place the marker at
-     * @param longitude the longitude to place the marker at
-     * @author Grant Fass
-     */
-    private void plotCoordinate(double latitude, double longitude) {
-        map.addMarker(Marker.createProvided(Marker.Provided.BLUE)
-                .setPosition(new Coordinate(latitude, longitude))
-                .setVisible(true));
+    private void displayRouteIDs(HashMap<Route, ArrayList<Stop>> hashMap) {
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for (Route route: hashMap.keySet()) {
+            items.add(route.getRouteID());
+        }
+        routeListView.setItems(items);
+    }
+
+    private void displayBusses(ArrayList<double[]> busLocations) {
+        int count = 0;
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for (double[] busLocation: busLocations) {
+            items.add("Bus: " + count);
+            count++;
+        }
+        busListView.setItems(items);
     }
 
     /**
-     * plots the specified marker
-     * @param marker the marker to plot
+     * Creates a CoordinateLine from the specified values
+     * @param route the route to get the default color from for the line
+     * @param stops the stops to plot on the CoordinateLine
+     * @param overrideDefaultColor will set the line to a random color if true
+     * @return the generated CoordinateLine
      * @author Grant Fass
      */
-    private void plotCoordinate(Marker marker) {
-        map.addMarker(marker);
-    }
-
-    /**
-     * Plots a coordinate line onto the map
-     * @param route the route that all of the stops are associated to (Used for route color)
-     * @param stops the stops to plot on the line
-     * @param overrideDefaultColors will override the default colors if true
-     * @author Grant Fass
-     */
-    private void plotCoordinateLine (Route route, ArrayList<Stop> stops, boolean overrideDefaultColors) {
-        final boolean fillRoutes = false;
-        final int routeWidth = 3;
+    private CoordinateLine getCoordinateLine(Route route, ArrayList<Stop> stops, boolean overrideDefaultColor) {
         final double red = Math.random();
         final double green = Math.random();
         final double blue = Math.random();
         final double alpha = 1;
         final Color color = new Color(red, green, blue, alpha);
-        /*
-         * Task to generate the coordinate line that is created from all of the stops associated
-         * with a single route
-         */
-        Task<CoordinateLine> task = new Task<>() {
-            @Override
-            protected CoordinateLine call() throws Exception {
-                ArrayList<Coordinate> coordinates = new ArrayList<>();
-                for (Stop stop : stops) {
-                    coordinates.add(new Coordinate(stop.getStopLatitude(), stop.getStopLongitude()));
-                }
-                CoordinateLine coordinateLine = new CoordinateLine(coordinates)
-                        .setVisible(true)
-                        .setWidth(routeWidth)
-                        .setClosed(fillRoutes)
-                        .setFillColor(Color.web("lawngreen", 0.5));
-                if (overrideDefaultColors) {
-                    coordinateLine.setColor(color);
-                } else {
-                    coordinateLine.setColor(route.getRouteColor());
-                }
-                return coordinateLine;
-            }
-        };
-        task.setOnSucceeded(e -> {
-            map.addCoordinateLine(task.getValue());
-            try {
-                map.setExtent(Extent.forCoordinates(task.getValue().getCoordinateStream().collect(Collectors.toList())));
-            } catch (IllegalArgumentException ignored) { }
-        });
-        new Thread(task).start();
+        ArrayList<Coordinate> coordinates = new ArrayList<>();
+        for (Stop stop : stops) {
+            coordinates.add(new Coordinate(stop.getStopLatitude(), stop.getStopLongitude()));
+        }
+        CoordinateLine coordinateLine = new CoordinateLine(coordinates).setVisible(true);
+        if (overrideDefaultColor) {
+            coordinateLine.setColor(color);
+        } else {
+            coordinateLine.setColor(route.getRouteColor());
+        }
+        return coordinateLine;
     }
 
     /**
-     * plots all of the stops associated with each route in a hashmap
-     * @param hashMap the route and stops combinations to plot
+     * plots a specified CoordinateLine on the map and changes the map view to contain the line
+     * @param coordinateLine the CoordinateLine to plot
      * @author Grant Fass
      */
-    private void plotStops(@NotNull HashMap<Route, ArrayList<Stop>> hashMap) {
-        for(Route route: hashMap.keySet()) {
-            plotCoordinateLine(route, hashMap.get(route), true);
+    private void plotCoordinateLine (CoordinateLine coordinateLine) {
+        List<Coordinate> coordinates = coordinateLine.getCoordinateStream().collect(Collectors.toList());
+        map.addCoordinateLine(coordinateLine);
+        try {
+            map.setExtent(Extent.forCoordinates(coordinates));
+        } catch (IllegalArgumentException ignored) {
         }
     }
 
     /**
-     * retrieves all of the stops that are associated with all routes
-     * @return a HashMap containing all of the Stops associated with all Routes
+     * removes the specified CoordinateLine from the map
+     * @param coordinateLine the CoordinateLine to remove
      * @author Grant Fass
      */
-    private HashMap<Route, ArrayList<Stop>> getStopsPerRoute() {
-        Data data = mainWindowController.getData();
-        //Create a list of all of the keys in StopTimes. Key Format = 'stop_id;trip_id'
-        String[] stopTimeKeys = data.getStopTimes().getStop_times().keySet().toArray(new String[0]);
-        //separate stopTimeKeys into stop_id values and trip_id values
-        ArrayList<String> stop_ids = new ArrayList<>();
-        ArrayList<String> trip_ids = new ArrayList<>();
-        for(String s:stopTimeKeys) {
-            stop_ids.add(s.substring(0, s.indexOf(';')));
-            trip_ids.add(s.substring(s.indexOf(';') + 1));
-        }
-        //convert stop_ids to Stops
-        ArrayList<Stop> stops = new ArrayList<>();
-        for(String stop_id:stop_ids) {
-            stops.add(data.getStops().getStop(stop_id));
-        }
-        //convert trip_ids to Routes
-        ArrayList<Route> routes = new ArrayList<>();
-        for(String trip_id:trip_ids) {
-            Trip trip = data.getTrips().getTrip(trip_id);
-            Route route = new Route("-1", "", "-1", "Null Route", "Null Route",
-                    "", "", Color.web("black").toString(), "",
-                    "", "", "");
-            if (trip != null) {
-                route = data.getRoutes().getRoute(trip.getRouteID());
-            }
-            routes.add(route);
-        }
-        //transfer data into HashMap
-        HashMap<Route, ArrayList<Stop>> stopsPerRoute = new HashMap<>();
-        for (int i = 0; i < routes.size(); i++) {
-            //If the Route already exists in the map then add the stop to the list of stops
-            //Otherwise create a new list of stops and add the route and stop to the map
-            //Do not include the stop if it already exists for a route
-            if (stopsPerRoute.containsKey(routes.get(i)) && !stopsPerRoute.get(routes.get(i)).contains(stops.get(i))) {
-                stopsPerRoute.get(routes.get(i)).add(stops.get(i));
-            } else if (routes.get(i) != null) {
-                ArrayList<Stop> stopsInRoute = new ArrayList<>();
-                stopsInRoute.add(stops.get(i));
-                stopsPerRoute.put(routes.get(i), stopsInRoute);
-            }
-        }
-        //Return Map
-        return stopsPerRoute;
+    private void removeCoordinateLine(CoordinateLine coordinateLine) {
+        map.removeCoordinateLine(coordinateLine);
     }
 
 
@@ -323,21 +256,58 @@ public class MapWindowController implements Observer {
      */
     @Override
     public void update(Data data) {
-//        try {
-            if (!data.getRoutes().getRoutes().isEmpty() && !data.getStops().getStops().isEmpty() && !data.getStopTimes().getStop_times().isEmpty() && !data.getTrips().getTrips().isEmpty()) {
-                if (Platform.isFxApplicationThread()) {
-                    plotStops(getStopsPerRoute());
-                } else {
-                    Platform.runLater(() -> plotStops(getStopsPerRoute()));
+        if (!data.getRoutes().getRoutes().isEmpty() && !data.getStops().getStops().isEmpty() && !data.getStopTimes().getStop_times().isEmpty() && !data.getTrips().getTrips().isEmpty()) {
+            updateRoutes(data);
+            updateBusses(data);
+        }
+    }
+
+    /**
+     * updates the displayed routes
+     * @param data the data to use to update the routes
+     * @author Grant Fass
+     */
+    private void updateRoutes(Data data) {
+        HashMap<Route, ArrayList<Stop>> stopsPerRoute = data.getStopsPerRoute();
+        displayRouteIDs(stopsPerRoute);
+        //Update the displayed route whenever a new route is clicked in the routeListView
+        routeListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            CoordinateLine lastCoordinateLine;
+
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                if (lastCoordinateLine != null) {
+                    removeCoordinateLine(lastCoordinateLine);
+                }
+                Route route = data.getRoutes().getRoute(newValue);
+                if (route != null) {
+                    lastCoordinateLine = getCoordinateLine(route, stopsPerRoute.get(route), false);
+                    plotCoordinateLine(lastCoordinateLine);
                 }
             }
-//        } catch (ConcurrentModificationException e) {
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException interruptedException) {
-//                interruptedException.printStackTrace();
-//            }
-//            update(data);
-//        }
+        });
+    }
+
+    /**
+     * plots a marker on the map at the selected bus location
+     * @param data the data class used to get the list of bus coordinates from
+     * @author Grant Fass
+     */
+    private void updateBusses(Data data) {
+        ArrayList<double[]> busCoordinates = data.getBusCoordinates();
+        displayBusses(busCoordinates);
+        final Marker marker = Marker.createProvided(Marker.Provided.ORANGE).setPosition(msoeAthleticField).setVisible(true);
+        map.addMarker(marker);
+        //Update the displayed bus whenever a new bus is clicked in the busListView
+        busListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                double[] coordinates = busCoordinates.get(Integer.parseInt(newValue.substring(5).trim()));
+                if (coordinates.length >= 2) {
+                    Coordinate coordinate = new Coordinate(coordinates[0], coordinates[1]);
+                    marker.setPosition(coordinate);
+                }
+            }
+        });
     }
 }
